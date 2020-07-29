41224ca1c9f6aace0007c44d1ebeea616ef14a8d
Kamil <kamilsalega@gmail.com>
2020-07-30 00:50:00 +0200

    KER-002 - Podłaczenie danych pod api

---
+++ car-parts-shop-client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <NavigationBar/>
+      <router-view />
+  </div>
+</template>
+
+<script>
+import NavigationBar from './components/NavigationBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar,
+  },
+};
+</script>
+
+<style >
+  body {
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+
+  main {
+    margin: 0 auto;
+    padding: 30px;
+    width: 1024px;
+    min-height: 300px;
+  }
+
+</style>
